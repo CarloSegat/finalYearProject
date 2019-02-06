19fd23454ee01a7d8b6c6272e268d820f4e3cc5d
Carlo <segatcarlo95@gmail.com>
2019-02-06 18:05:04 +0000

    done stuff

--- my/carlo.py
+++ my/carlo.py
@@ -3,7 +3,6 @@ import re
 import numpy as np
 import pdb
 from keras.preprocessing import sequence
-# what is it?
 from keras.regularizers import l2
 from keras.models import Model
 from keras.layers.merge import concatenate
@@ -18,64 +17,50 @@ from IPython.display import display
 import itertools
 from nltk.corpus import words
 import matplotlib.pyplot as plt
-
-### Read the glove word vectors (space delimited strings) into a dictionary from word->vector
+from utils import *
+from gensim.models import *
 
 ### Set parameters
-embed_size   = 50    # how big is each word vector
+embed_size   = 300    # how big is each word vector
 max_features = 20000 # how many unique words to use (i.e num rows in embedding vector)
 maxlen       = 100   # max number of words in a comment to use
-myActivationFunction = 'tanh'
+myActivationFunction = 'tanh'	
 
 ### Load data
-train = pd.read_csv('train.csv') 
-test = pd.read_csv('test.csv') 
-list_sentences_train = train["comment_text"].fillna("_NaN_").values
-list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-y = train[list_classes].values # [[0,0,0,0,0][...]]
-list_sentences_test = test["comment_text"].fillna("_NaN_").values
-### Convert sentences to padded list of integers
+list_sentences_train = loadSentences('2017ATrain.txt')
+y = loadTargets('2017ATrain.txt')	
+assert(y.shape[0] == len(list_sentences_train))
+list_classes = ["positive", "neutral", "negative"]
+
 tokenizer = Tokenizer(num_words=max_features)
 tokenizer.fit_on_texts(list(list_sentences_train))
-# The Tokenizer stores everything in the word_index during fit_on_texts. 
-# Then, when calling the texts_to_sequences method, only the top num_words are considered.
-# i.e. if list_sentences_train has more than num_word eords in excess wont be in the word_index and
-# they will be ignored when calling texts_to_sequences
 list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
-list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+#list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
 X_train = pad_sequences(list_tokenized_train, maxlen=maxlen, padding='post')
-pdb.set_trace()
-X_test = pad_sequences(list_tokenized_test, maxlen=maxlen, padding='post')
+#X_test = pad_sequences(list_tokenized_test, maxlen=maxlen, padding='post')
 
-### Read a list of 40.000 embeddings, word --> vector of floats
-embeddings_index = {}
-for o in open('./glove.6B.50d.txt', encoding="utf8"):
-	embeddings_index[o.strip().split()[0]] = np.asarray(o.strip().split()[1::], dtype='float32')
-
-
-### Create embeddings matrix
-all_embs = np.stack(embeddings_index.values())
-emb_mean,emb_std = all_embs.mean(), all_embs.std()
+googleModel = KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)  
+wv = googleModel.wv
 
 word_index = tokenizer.word_index # Word index stores the words we have tokenised, most common words have higher indexes
 nb_words = min(max_features, len(word_index))
 
 # Initialize embedding matrix with random arrays whose elemnts are drawn from a normal distribution with 
 # parameters based on the predefined embedding values
-embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size)) 
+embedding_matrix = np.random.normal(0, 3, (nb_words, embed_size)) 
 
 # Loop through each word and get its embedding vector
 for word, wordIndex in word_index.items(): # list of tuples
 	if wordIndex >= max_features:
 		continue # Skip words appearing less than the minimum allowed
-	embedding_vector = embeddings_index.get(word)
-	if embedding_vector is not None: 
+	try:
+		embedding_vector = wv[word]
 		embedding_matrix[wordIndex] = embedding_vector
+	except KeyError:
+		embedding_matrix[wordIndex] = embedding_matrix[wordIndex]
 		
 ## ????????????
 conv_filters = 128 # No. filters to use for each convolution
-weight_vec = list( np.max(np.sum(y, axis=0)) / np.sum(y, axis=0) ) # axis 0 is vertical
-class_weight = {i: weight_vec[i] for i in range(6)}
 
 
 sampleLength = X_train.shape[1] # 100 as specified in max length
@@ -111,7 +96,7 @@ drp1 = Dropout(0.2)(cnct)
 dns1  = Dense(32, activation='relu')(drp1)
 btch1 = BatchNormalization()(dns1)
 drp2  = Dropout(0.2)(btch1)
-out = Dense(y.shape[1], activation='sigmoid')(drp2)
+out = Dense(y.shape[1], activation='sigmoid')(drp2) # output array of 3 probabilities
 
 # Compile
 model = Model(inputs=inp, outputs=out)
@@ -120,8 +105,8 @@ model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 #pdb.set_trace()
 
 # Estimate model
-model.fit(X_train, y, validation_split=0.1, epochs=2, batch_size=32, shuffle=True, class_weight=class_weight)
-
+model.fit(X_train, y, validation_split=0.2, epochs=10, batch_size=64, shuffle=True)
+pdb.set_trace()
 # Predict
 preds = model.predict(X_test)
 

Fixed softmax

1-ADC/AttentionTopic/main.py
@@ -1,6 +1,6 @@
 import sklearn
 
-from keras import Input, Sequential, Model
+from keras import Input, Sequential, Model, metrics
 from keras.activations import softmax
 from keras.backend import l2_normalize, dot
 from keras.engine import Layer
@@ -17,9 +17,12 @@ from SemEval import SemEvalData
 
 
 # Paper Parameters
+from loss import focal_loss
+from utils import split_train_x_y_and_validation, get_early_stop_callback
+
 EMB_DIMENSIONS = 300
 VALIDATION_PERCENTAGE = 0.1
-TOPICS = 12
+TOPICS = 11
 DROPOUT = 0.6
 MAX_SEQUENCE_LENGTH = 80
 GRU_HIDDEN_SIZE = 128
@@ -33,7 +36,7 @@ MAX_EPOCHS = 300
 PATIENCE = 20
 
 # My Parameters
-GRU_RECURRENT_DROPOUT = 0.2
+GRU_RECURRENT_DROPOUT = 0.3
 def stack(probs):
     return tf.stack(probs, axis=1)
 
@@ -42,15 +45,20 @@ def squash(vectors, axis=-1):
     scale = s_squared_norm / (1 + s_squared_norm) / K.sqrt(s_squared_norm)
     return scale * vectors
 
+def binary_accuracy(y_true, y_pred):
+    r = K.print_tensor(K.round(y_pred), "ROund = ")
+    return K.mean(K.equal(y_true, r), axis=-1)
+
 def get_norm(vector):
-    return norm(vector, ord=2, axis=1)
+    n = K.print_tensor(norm(vector, ord=2, axis=1), "Norm of last vector = ")
+    return n
 
 def paper_regulariser(weights):
     normalised_weights = weights / tf.sqrt(tf.reduce_sum(tf.square(weights), axis=0, keepdims=True))
     dot_prod_between_topic_matrices = tf.matmul(tf.transpose(normalised_weights), normalised_weights)
     dot_prod_between_topic_matrices = K.print_tensor(dot_prod_between_topic_matrices,
                    "dot_prod_between_topic_matrices = ")
-    minus_identity_matrix = dot_prod_between_topic_matrices - tf.eye(12)
+    minus_identity_matrix = dot_prod_between_topic_matrices - tf.eye(11)
     absolute_value = tf.abs(minus_identity_matrix)
     sum_columns = reduce_sum(absolute_value, axis=0)
     sum_all_elements = reduce_sum(sum_columns )
@@ -58,7 +66,7 @@ def paper_regulariser(weights):
 
 class My_Attention(Layer):
 
-    def __init__(self, topics=12, **kwargs):
+    def __init__(self, topics=11, **kwargs):
         self.topics = topics
         self.output_dim = 256
         super(My_Attention, self).__init__(**kwargs)
@@ -78,22 +86,22 @@ class My_Attention(Layer):
         importance_all_words_for_all_topics = matmul(reshaped_x, self.kernel)
         # All word embeddigns multiplied with all topic embeddings
         vs = []
-        for t in range(12):
+        for t in range(self.topics):
             importance_all_words_topic_t = reshape(importance_all_words_for_all_topics[:, t],
                                                    (80, -1))
 
+            importance_all_words_topic_t = softmax(importance_all_words_topic_t, axis=0)
             topic_weight_multiplier = tile(reshape(importance_all_words_topic_t,
                                                    (80 * tf.shape(x)[0], 1)), (1, 256))
-            topic_weight_multiplier = softmax(topic_weight_multiplier, axis=0)
             r = topic_weight_multiplier * reshaped_x
             r = reshape(r, (80, -1))
-            sum = reduce_sum(r, axis=0)
+            sum = reduce_sum(r, axis=0, keep_dims=True)
             v_on_rows = reshape(sum, (tf.shape(x)[0], 256))
             vs.append(v_on_rows)
         return vs
 
     def compute_output_shape(self, input_shape):
-        return [(None, input_shape[2]) for i in range(12)]
+        return [(None, input_shape[2]) for i in range(self.topics)]
 
 def make_model():
     # We don't need to specify the max length of the sequence, therefore None
@@ -105,33 +113,39 @@ def make_model():
     m = My_Attention()(m)
     topic_squash = []
     for i in range(TOPICS):
-        d = Dense(NEURONS_MLP_SQUASH_1_2016, activation='tanh', kernel_initializer=glorot_uniform())(m[i])
-        d = Dropout(DROPOUT)(d)
+        d = Dropout(DROPOUT)(m[i])
+        d = Dense(NEURONS_MLP_SQUASH_1_2016, activation=None,
+                  kernel_initializer=glorot_uniform())(d)
         d = Lambda(squash)(d)
         topic_squash.append(d)
     m = concatenate((topic_squash), axis=1)
+    m = Dropout(DROPOUT)(m)
     assert(m.shape[1] == NEURONS_MLP_SQUASH_1_2016 * TOPICS)
     p = []
-    for i in range(TOPICS):
-        d = Dense(NEURONS_MLP_SQUASH_2_2016, activation='tanh', kernel_initializer=glorot_uniform())(m)
-        d = Dropout(DROPOUT)(d)
+    for i in range(TOPICS+1):
+        d = Dense(NEURONS_MLP_SQUASH_2_2016, activation=None, kernel_initializer=glorot_uniform())(m)
         d = Lambda(squash)(d)
         d = Lambda(get_norm)(d)
         p.append(d)
     m = Lambda(stack)(p)
     model = Model(sequence_input, m)
-    opti = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True, clipvalue=0.5)#adam()
+    opti = adam()#SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True, clipvalue=0.5)
 
-    model.compile(optimizer=opti, loss='mean_squared_error', metrics=[])
+    model.compile(optimizer=opti, loss='mean_squared_error', metrics=[metrics.binary_accuracy])
     return model
 
 
 s = SemEvalData()
 embedding = Komn(s.make_vocabulary())
-x_train, y_train, x_test, y_test = s.get_x_embs_and_y_onehot(embedding)
+x_train_val, y_train_val, x_test, y_test = s.get_x_embs_and_y_onehot(embedding)
+x_train, y_train, validation = split_train_x_y_and_validation(0.1,
+                                                            x_train_val, y_train_val)
 model = make_model()
 print(model.summary())
 model.fit(x_train, y_train, batch_size=BATCH_SIZE,
-          epochs=100, validation_split=VALIDATION_PERCENTAGE)
+          epochs=50, validation_data=validation, callbacks=[get_early_stop_callback()])
 pred_test = model.predict(x_test)
-print(sklearn.metrics.f1_score(y_test, pred_test, average='micro'))
+#print(sklearn.metrics.f1_score(y_test, pred_test, average='micro'))
+
+# Validation_loss of 0.3 with cross entropy
+# V loss of o.15 with MSE

SemEval.py
@@ -1,5 +1,6 @@
 import os
 import re
+import string
 import xml
 from pathlib import Path
 from keras.utils import to_categorical
@@ -13,23 +14,25 @@ from Embeddings import Komn
 from TextPreprocessor import TextPreprocessor
 from utils import dump_gzip, load_gzip, ROOT_DIR, make_tokenizer, check_argument_is_numpy, pad_array
 
-TRAIN_SENTENCES = 2000
-TEST_SENTENCES = 676
+NO_OPINION_TRAIN = 292
+NO_OPINION_TEST = 89
+TRAIN_SENTENCES = 2000 - NO_OPINION_TRAIN
+TEST_SENTENCES = 676 - NO_OPINION_TEST
 TRAIN_OPINIONS = 2507
 TEST_OPINIONS = 859
 
-ASPECT_CATEGORIES = {'DRINKS#STYLE_OPTIONS': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                     'LOCATION#GENERAL': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-                     'AMBIENCE#GENERAL': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     'FOOD#PRICES': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                     'FOOD#QUALITY': [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-                     'RESTAURANT#PRICES': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                     'DRINKS#QUALITY': [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     'DRINKS#PRICES': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     'FOOD#STYLE_OPTIONS': [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                     'RESTAURANT#GENERAL': [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-                     'SERVICE#GENERAL': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     'RESTAURANT#MISCELLANEOUS': [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+ASPECT_CATEGORIES = {'DRINKS#STYLE_OPTIONS': [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'LOCATION#GENERAL': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                     'AMBIENCE#GENERAL': [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'FOOD#PRICES': [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'FOOD#QUALITY': [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'RESTAURANT#PRICES': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                     'DRINKS#QUALITY': [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'DRINKS#PRICES': [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'FOOD#STYLE_OPTIONS': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                     'RESTAURANT#GENERAL': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                     'SERVICE#GENERAL': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                     'RESTAURANT#MISCELLANEOUS': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]
                      }
 
 class SemEvalData():
@@ -94,7 +97,7 @@ class SemEvalData():
             train_x_train_y_test_x_test_y.append(np.array(y, dtype=np.int32))
         return train_x_train_y_test_x_test_y
 
-    def get_x_embs_and_y_onehot(self, embedding, pad=True, pad_size=80):
+    def get_x_embs_and_y_onehot(self, embedding, pad=True, pad_size=80, balanced_validation=False):
 
         def assert_is_one_hot_vector(multiclass_label):
             check_argument_is_numpy(multiclass_label)
@@ -134,6 +137,7 @@ class SemEvalData():
         ''' Creates files of this format:
         {sentence_id: { 'sentence':[sentence], 'opinions':[{}, {}]}'''
 
+        removed = 0
         for file_in, file_out in [(self.path_train_raw, self.path_train_ready),
                                   (self.path_test_raw, self.path_test_ready)]:
             opinion_count = 0
@@ -144,7 +148,6 @@ class SemEvalData():
                     for sentence in sentences:
                         ready[sentence.get("id")] = {}
                         this_id = sentence.get("id")
-                        this_sentence = ''
                         for data in sentence:
                             if data.tag == "text":
                                 this_sentence = data.text
@@ -161,6 +164,10 @@ class SemEvalData():
                                         'polarity': opinion.get("polarity"),
                                         'to': int(opinion.get("to")),
                                         'from': int(opinion.get("from")) })
+                        if len(ready[this_id]['opinions']) == 0:
+                            del ready[this_id] # remove sentence with no opinions
+                            removed += 1
+
             # sanity check: we got as many opinion tuples as the SemEval paper says
             assert(opinion_count == TEST_OPINIONS or opinion_count == TRAIN_OPINIONS)
             dump_gzip(ready, file_out)
@@ -168,7 +175,8 @@ class SemEvalData():
         self.ready_train = load_gzip(self.path_train_ready)
         self.ready_test = load_gzip(self.path_test_ready)
         # sanity check: we got as many sentences as the SemEval paper says
-        assert (len(self.ready_train) == TRAIN_SENTENCES and len(self.ready_test) == TEST_SENTENCES)
+        assert (len(self.ready_train) + len(self.ready_test) + removed ==
+                TEST_SENTENCES + TRAIN_SENTENCES)
 
     def get_test_sentences(self):
         s =  self.get_sentences(self.ready_test)
@@ -181,69 +189,57 @@ class SemEvalData():
         return s
 
     def get_sentences(self, data):
-        list_dictionaries = data.values()
-        sentences = [dictio['sentence'] for dictio in list_dictionaries]
+        list_dictionaries = list(data.values())
+        sentences = []
+        for d in list_dictionaries:
+            try:
+                sentences.append(['sentence'])
+            except Exception:
+                pass
         return np.array(sentences)
 
+    def prepare_file_for_Stanford_parser(self):
+        ''' Standford dependency parser wants a file where sentences are
+        separated by a full stop'''
+        sentences = self.get_all_sentences()
+        for i in range(len(sentences)):
+            if sentences[i][-1] != '.' and sentences[i][-1] != '!' \
+                    and sentences[i][-1] != '?':
+                sentences[i] = np.append(sentences[i], '.')
+        sentences = [' '.join(s) for s in sentences]
+        f = open("inputStanNLP.txt", 'w')
+        for s in sentences:
+            f.write(s)
+            f.write('\n')
+        f.close()
 
 
+    def prepare_tagged_sentences(self, path='rawTextToParse.xml'):
+        '''Takes as input a xml file containing standford annotated sentences.
+        Such file can be obtained by calling the following command:
+        java edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,depparse -file <INPUT_FILE>
 
-
-def multiHotVectors(categories , name):
-    #http://stackoverflow.com/questions/18889588/create-dummies-from-column-with-multiple-values-in-pandas
-    dummies = pd.get_dummies(categories['category'])
-    atom_col = [c for c in dummies.columns if '*' not in c]
-    for col in atom_col:
-        categories[col] = dummies[[c for c in dummies.columns if col in c]].sum(axis=1)
-
-    categories.to_csv(name+".csv")
-    return categories
-
-def multiCategoryVectors(dataset, classes=12):
-    labels = np.zeros((dataset.shape[0], classes))  # matrix of zeroes for accross all example labels
-    for i in range(dataset.shape[0]):
-        vec = dataset.values[i][2:14]
-        labels[i] = vec
-    print("Hot encoded vectors shape: ", labels.shape)
-    return labels
-
-def get_SOW(sentence, embeddings):
-    # beacuse we are summing: if word is not in embeddings we just append a vector of zeroes
-    sentence = [embeddings[w] if w in embeddings.keys()
-                else np.zeros((1, len(embeddings["tree"]))) for w in sentence]
-    running_sum = np.zeros((1, len(sentence[0])))
-    for emb in sentence:
-        running_sum = np.add(running_sum, emb)
-    return running_sum
-
-def get_data():
-    training_output_File = 'train.txt'
-    gold_output_File = 'test.txt'
-
-    train = pd.read_csv(training_output_File, header=None, delimiter="\t", quoting=3, names=['review', 'category'])
-    test = pd.read_csv(gold_output_File, header=None, delimiter="\t", quoting=3, names=['review', 'category'])
-    train = multiHotVectors(train, 'test_labels')
-    test = multiHotVectors(test, 'train_labels')
-
-    x_train = train["review"]
-    y_train = np.array(multiCategoryVectors(train, classes=12)).astype(int)
-    x_test = test["review"]
-    y_test = np.array(multiCategoryVectors(test, classes=12)).astype(int)
-
-    embs = load_gzip("Yelp_word_to_emb")
-
-    #x_train = [preprocess(s, return_list=True) for s in x_train]
-    x_train = [get_SOW(x, embs) for x in x_train]
-
-    #x_test = [preprocess(s, return_list=True) for s in x_test]
-    x_test = [get_SOW(x, embs) for x in x_test]
-    return np.squeeze(np.asarray(x_train[:])), y_train, \
-           np.squeeze(np.asarray(x_test[:])), y_test
-
-def pad_punctuation_spaces(s):
-    s = re.sub('([.,!?()])', r' \1 ', s)
-    s = re.sub('\s{2,}', ' ', s)
-    return s
+        Where input file contain all the train and test sentences, preprocessed such
+        that they always end with either . ! or ?'''
+        opinion_count = 0
+        ready = {}
+        docs = xml.etree.ElementTree.parse(path).getroot()
+        for doc in docs:
+            for sentences in doc:
+                tagged_sentences = []
+                for s in sentences:
+                    for dependencies in s:
+                        if dependencies.tag == 'dependencies':
+                            if dependencies.attrib['type'] == 'basic-dependencies':
+                                build = []
+                                for dep in dependencies:
+                                    tag = dep.attrib['type']
+                                    for e in dep:
+                                        if e.tag == 'dependent':
+                                            build.append(tag + "_" + e.text)
+                                tagged_sentences.append(build)
+        assert(len(tagged_sentences) == TRAIN_SENTENCES + TEST_SENTENCES)
+        dump_gzip(tagged_sentences, 'all_tagged_senences')
 
 def format_xml_for_NER():
 
@@ -289,6 +285,8 @@ def format_xml_for_NER():
 
 if __name__ == '__main__':
     s = SemEvalData()
+    s.prepare_tagged_sentences()
+    s.prepare_file_for_Stanford_parser()
     r = s.make_vocabulary()
     g = Komn(s.make_vocabulary())
     xt, yt, xte, yte = s.get_x_embs_and_y_onehot(g)

loss.py
@@ -1,7 +1,7 @@
 import sklearn
 from keras import backend as K
 from keras.backend import categorical_crossentropy
-
+import tensorflow as tf
 
 def weighted_categorical_crossentropy(weights):
     """
@@ -61,3 +61,12 @@ def micro_f1(y_true, y_pred):
 
 def cat_crossentropy_from_logit(target, output,):
     return categorical_crossentropy(target, output, from_logits=True)
+
+def focal_loss(y_true, y_pred):
+    gamma = 2.0
+    alpha = 0.25
+    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma)
+                  * K.log(pt_1))-K.sum((1-alpha)
+                  * K.pow( pt_0, gamma) * K.log(1. - pt_0))

tensorflow_playground.py
@@ -1,8 +1,17 @@
 import numpy as np
 import tensorflow as tf
 from keras.activations import softmax
-from tensorflow import einsum, reshape, matmul, tile, reduce_sum, transpose
+from tensorflow import einsum, reshape, matmul, tile, reduce_sum, transpose, norm
 from tensorflow.python.client import session
+import keras.backend as K
+
+def squash(vectors, axis=-1):
+    s_squared_norm = K.sum(K.square(vectors), axis, keepdims=True)
+    scale = s_squared_norm / (1 + s_squared_norm) / K.sqrt(s_squared_norm)
+    return scale * vectors
+
+def get_norm(vector):
+    return norm(vector, ord=2, axis=0)
 
 def length_vec(x):
     r = [a*a for a in x]
@@ -40,6 +49,12 @@ nlw =  tf.constant([
 t = tf.constant([0.5, 0.5, 0.5, 0.5, 0.5])
 
 with tf.Session() as sess:
+    rang = transpose(tf.range(0.0, 256.0, 1.0))
+    r = sess.run(squash(t))
+    rr = sess.run(squash(rang))
+    r = get_norm(r)
+    rr = get_norm(rr)
+
     test_weights = tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32)
     test_weights = tf.stack([test_weights,tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32),tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32),test_weights,
               tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32),tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32),tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32),tf.convert_to_tensor(np.random.rand(256), dtype=tf.float32),

utils.py
@@ -1,25 +1,13 @@
-import re
+from math import ceil
+from random import shuffle
 import numpy as np
-import pdb
+from keras import callbacks
 from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from gensim.models import *
-from ekphrasis.classes.preprocessor import TextPreProcessor
-from ekphrasis.classes.tokenizer import SocialTokenizer
-from ekphrasis.dicts.emoticons import emoticons
-from ekphrasis.classes.segmenter import Segmenter
-from ekphrasis.classes.spellcorrect import SpellCorrector
-from nltk.corpus import stopwords
-import nltk
-from collections import Counter
 import pickle as pkl
 import gzip
 import os
-import sys
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
 
 def make_tokenizer(sentences, wordsUsed):
     tokenizer = Tokenizer(wordsUsed)
@@ -51,3 +39,48 @@ def pad_array(arr, how_much):
         arr.append(p)
     assert(len(arr) == how_much)
     return arr
+
+def split_train_x_y_and_validation(validation_size, x_train, y_train, classes=12):
+
+    def get_classes_of_label(multi_label_vector):
+        return np.where(multi_label_vector == 1)[0]
+
+    how_many_of_each_class = ceil(len(x_train) * validation_size / classes)
+    classes = dict(enumerate([0] * classes))
+    d = list(zip(x_train, y_train))
+    shuffle(d)
+    train_x = []
+    train_y = []
+    validation_x = []
+    validation_y = []
+
+    for pair in d:
+        put_in_validation = True
+        for klass in get_classes_of_label(pair[1]):
+            if classes[klass] > how_many_of_each_class:
+                train_x.append(pair[0])
+                train_y.append(pair[1])
+                put_in_validation = False
+                break
+            else:
+                continue
+        if put_in_validation:
+            for klass in get_classes_of_label(pair[1]):
+                classes[klass] += 1
+            validation_x.append(pair[0])
+            validation_y.append(pair[1])
+
+    return np.array(train_x), np.array(train_y), \
+           (np.array(validation_x), np.array(validation_y))
+
+def get_early_stop_callback():
+    return callbacks.EarlyStopping(monitor='val_loss',
+                              min_delta=0,
+                              patience=10,
+                              verbose=0, mode='auto',
+                              restore_best_weights=True)
+
+
+
+
+
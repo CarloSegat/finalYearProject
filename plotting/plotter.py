0251319f04047f8d90f84e2d5d94ebe158b116a0
CarloSegat <segatcarlo95@gmail.com>
2019-04-22 12:22:08 +0100

    Update plotter.py
    new plotter with legend and F1 mean

--- plotting/plotter.py
+++ plotting/plotter.py
@@ -1,31 +1,56 @@
 import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-import scipy.stats
+import scipy.stats, sys
+sys.path.append('..\.')
 
 from utils import load_gzip
-
-def plot(data, name):
-    kde = scipy.stats.gaussian_kde(data,bw_method=None)
-    t_range = np.linspace(-2,8,200)
-    plt.plot(t_range,kde(t_range),lw=2)
-    plt.xlim(0,1)
-    plt.legend(loc='best')
-    #plt.show()
-
-data = load_gzip('FF-ACD-Results')
 
 embs = ['komn', 'google', 'yelp']
 synt = ['synt', 'no-synt']
 stop = ['stop-kept', 'stop-removed']
 punct = ['punct-kept', 'punct-removed']
 
+
+def get_syntax_and_noSyntax_mean(data):
+    syntax, no_syntax = 0, 0
+    for e in embs:
+        for s in synt:
+            for st in stop:
+                for p in punct:
+                    if s == 'no-synt':
+                        no_syntax += sum(data[e][s][st][p])
+                    else:
+                        syntax += sum(data[e][s][st][p])
+    return format(syntax / 12*10, '.4f') + '  ' +  format(no_syntax / 12*10, '.4f')
+
+def plot(data, name):
+    kde = scipy.stats.gaussian_kde(data,bw_method=None)
+    t_range = np.linspace(-2,8,200)
+
+    plt.plot(t_range,kde(t_range),lw=2, label=name)
+    plt.xlim(0,1)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2)
+    #plt.show()
+
+data = load_gzip('FF-ACD-Results')
+
+
+new_legend = {'stop-kept': 'stopwords', 
+              'stop-removed': 'no-stopwprds',
+              'punct-kept': 'punctuation',
+              'punct-removed': 'no-punctuation',
+              'synt': 'syntax', 'no-synt': 'no-syntax'}
+
 for e in embs:
     for s in synt:
         for st in stop:
             for p in punct:
-                plot(data[e][s][st][p], e+ ' '+s+ ' '+st+ ' '+p)
-    plt.savefig(e + '.png')
+                legend = e+ ' '+new_legend[s]+ ' '+new_legend[st]+ ' '+new_legend[p]
+                plot(data[e][s][st][p], legend)
+    #plt.tight_layout()
+    plt.figtext(.15, .80, 'F1 mean for syntax and no-syntax:\n' + get_syntax_and_noSyntax_mean(data))
+    plt.savefig('img/'+ e + '.png', figsize=(100,50), bbox_inches='tight')
     plt.clf()
     plt.cla()
     plt.close()
